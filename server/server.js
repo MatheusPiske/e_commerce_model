cbeb7fa357bb4f9c3a722ca9b8ff23ade004bb97
Matheus Henrique <matheus.piske14b@gmail.com>
2024-05-02 14:14:15 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cors({ origin: true, credentials: true }));
 
-const stripe = require("stripe")("private_id");
+const stripe = require("stripe")("private_key");
 
 app.post("/checkout", async (req, res, next) => {
     try {
